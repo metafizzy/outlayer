8fc008c25340585b878b40731e395130eb28f1e6
David DeSandro <david@desandro.com>
2013-05-03 08:12:22 -0400

    test jquery plugin

---
+++ test/jquery-plugin.js
@@ -0,0 +1,18 @@
+( function() {
+
+'use strict';
+
+var $ = window.jQuery;
+
+test( 'jQuery plugin', function() {
+  var $elem = $('#jquery');
+  ok( $.fn.cellsByRow, '.cellsByRow is in jQuery.fn namespace' );
+  equal( typeof $elem.cellsByRow, 'function', '.cellsByRow is a plugin' );
+  $elem.cellsByRow();
+  var layout = $elem.data('cellsByRow');
+  ok( layout, 'CellsByRow instance via .data()' );
+  equal( layout, CellsByRow.data( $elem[0] ), 'instance matches the same one via CellsByRow.data()' );
+
+});
+
+})();
