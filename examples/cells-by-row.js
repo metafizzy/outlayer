5fff997710c2d75e5856bab4ed8b3f8a27eaf621
David DeSandro <david@desandro.com>
2013-05-06 21:17:12 -0400

    messing with layoutQueue
    improves performance by separating getting item size, and setting item position

--- examples/cells-by-row.js
+++ examples/cells-by-row.js
@@ -37,15 +37,21 @@ CellsByRow.prototype.layoutItems = function( items, isInstant ) {
     this.emitEvent( 'layoutComplete', [ this, items ] );
   });
 
+  // getItemSizes( items );
+  this._layoutQueue = [];
+
   for ( var i=0, len = items.length; i < len; i++ ) {
     var item = items[i];
+    item.getSize();
     var column = this.itemIndex % this.cols;
     var row = Math.floor( this.itemIndex / this.cols );
     var x = column * this.columnWidth;
     var y = row * this.rowHeight;
-    this._layoutItem( item, x, y, isInstant );
+    this._queueItemLayout( item, x, y, isInstant );
     this.itemIndex++;
   }
+
+  this._processLayoutQueue();
 
   // set container size
   var elemH = Math.ceil( this.itemIndex / this.cols ) * this.rowHeight;
@@ -58,5 +64,27 @@ CellsByRow.prototype.layoutItems = function( items, isInstant ) {
   this.element.style.height = elemH + 'px';
 
 };
+
+CellsByRow.prototype._queueItemLayout = function( item, x, y, isInstant ) {
+  this._layoutQueue.push({
+    item: item,
+    x: x,
+    y: y,
+    isInstant: isInstant
+  });
+};
+
+CellsByRow.prototype._processLayoutQueue = function() {
+  for ( var i=0, len = this._layoutQueue.length; i < len; i++ ) {
+    var obj = this._layoutQueue[i];
+    this._layoutItem( obj.item, obj.x, obj.y, obj.isInstant );
+  }
+};
+
+// function getItemSizes( items ) {
+//   for ( var i=0, len = items.length; i < len; i++ ) {
+//     items[i].getSize();
+//   }
+// }
 
 })( window );
