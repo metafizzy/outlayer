bfdd9d77953b3bc69b163350524bff92d7e967e9
David DeSandro <desandrocodes@gmail.com>
2013-05-26 12:28:12 -0400

    check namespace is unchanged

--- test/create.js
+++ test/create.js
@@ -8,6 +8,7 @@ test( 'create CellsByRow', function() {
 
   equal( typeof CellsByRow, 'function', 'CellsByRow is a function' );
   equal( CellsByRow.prototype.settings.namespace, 'cellsByRow', 'cellsByRow namespace' );
+  equal( Outlayer.prototype.settings.namespace, 'outlayer', 'Outlayer namespace unchanged' );
   equal( CellsByRow.prototype.options.isResizeBound, true, 'isResizeBound option there' );
   equal( CellsByRow.prototype.options.columnWidth, 100, 'columnWidth option set' );
 
