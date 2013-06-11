68456ea68c3069ae8573414ad0595185e1017841
David DeSandro <desandrocodes@gmail.com>
2013-06-11 07:31:46 -0400

    test destroy

---
+++ test/destroy.js
@@ -0,0 +1,25 @@
+test( 'destroy', function() {
+
+  'use strict';
+
+  var container = document.querySelector('#destroy');
+  var layout = new CellsByRow( container );
+
+  layout.destroy();
+
+  function checkStyle( elem, property ) {
+    ok( !elem.style[ property ], elem + ' has no ' + property + ' style' );
+  }
+
+  checkStyle( container, 'height' );
+  checkStyle( container, 'position' );
+
+  var items = container.querySelectorAll('.item');
+  for ( var i=0, len = items.length; i < len; i++ ) {
+    var itemElem = items[i];
+    checkStyle( itemElem, 'position' );
+    checkStyle( itemElem, 'left' );
+    checkStyle( itemElem, 'top' );
+  }
+
+});
