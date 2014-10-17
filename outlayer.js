016cda8752c566bcc7254e29045a10f4c83e9080
Travis McKinney <travis.r.mckinney@gmail.com>
2014-10-17 15:57:00 -0400

    use wolfy87-eventemitter

--- outlayer.js
+++ outlayer.js
@@ -1000,7 +1000,7 @@ if ( typeof define === 'function' && define.amd ) {
     require('jquery'),
     require('eventie'),
     require('doc-ready'),
-    require('eventemitter'),
+    require('wolfy87-eventemitter'),
     require('get-size'),
     require('matches-selector'),
     require('./item')
